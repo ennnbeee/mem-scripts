9e00fd40f5a0d23f1e43d90ae13514638b24cd3e
Nick Benton <Nick-Benton@phoenixs.co.uk>
2022-03-17 15:33:25 +0000

    Updated BitLocker WinRE with logic

--- BitLocker/Enable-Bitlocker/Enable-WinRE_Bitlocker.ps1
+++ BitLocker/Enable-Bitlocker/Enable-WinRE_Bitlocker.ps1
@@ -47,106 +47,121 @@ try {
     Write-Verbose -Message "STARTING POINT:  Get WinRE Status for $OSDrive"
     $WinREStatus = reagentc.exe /info
     Write-Verbose -Message "$WinREStatus"
-
-    if($WinREStatus -like '*Windows RE status:         Disabled*'){
-        Write-Verbose -Message "WinRE Partion Enabled"
+    $HotToTrot ="false"
+    #Checks Windows Recovery Environment and enables if disabled
+    if($WinREStatus -like '*Windows RE status:         Enabled*'){
+        $HotToTrot = "True"
+        Write-Verbose -Message "WinRE Partion Enabled and good to enable BitLocker $HotToTrot"
     }
     Else{
         Try{
-            reagentc.exe /enable
-            Write-Verbose -Message "WinRE Enabled"
+            $WinREEnable = reagentc.exe /enable
+            if($WinREEnable -like '*Operation Successful*'){
+                $HotToTrot = "True"
+                Write-Verbose -Message "WinRE Partion Enabled and good to enable BitLocker, HotToTrot set to $HotToTrot"
+            }
+            Else{
+                $HotToTrot ="false"
+                Write-Verbose -Message "Unable to enabled WinRE, HotToTrot set to $HotToTrot"
+            }
         }
         Catch{
+            $HotToTrot ="false"
             Write-Verbose -Message "Unable to enabled WinRE"
         }
     }
+    if($HotToTrot -eq 'True'){
 
-    Write-Verbose -Message "Get-BitLockerVolume $OSDrive"
-    
-    # Evaluate the Volume Status to see what we need to do...
-    $bdeProtect = Get-BitLockerVolume -MountPoint $OSDrive | Select-Object -Property "VolumeStatus", "KeyProtector"
+        Write-Verbose -Message "Get-BitLockerVolume $OSDrive"
+        
+        # Evaluate the Volume Status to see what we need to do...
+        $bdeProtect = Get-BitLockerVolume -MountPoint $OSDrive | Select-Object -Property "VolumeStatus", "KeyProtector"
 
-    # Account for an uncrypted drive 
-    if ($bdeProtect.VolumeStatus -eq "FullyDecrypted" -or $bdeProtect.KeyProtector.Count -lt 1) {
-        
-        # Enable Bitlocker using TPM
-        Write-Verbose -Message "Enabling BitLocker due to FullyDecrypted status or KeyProtector count less than 1"
-        Enable-BitLocker -MountPoint $OSDrive -TpmProtector -SkipHardwareTest -UsedSpaceOnly -ErrorAction "Continue"
-        Enable-BitLocker -MountPoint $OSDrive -RecoveryPasswordProtector -SkipHardwareTest
-    }  
-    elseif ($bdeProtect.VolumeStatus -eq "FullyEncrypted" -or $bdeProtect.VolumeStatus -eq "UsedSpaceOnly") {
-        
-        # $bdeProtect.ProtectionStatus -eq "Off" - This catches the Wait State
-        if ($bdeProtect.KeyProtector.Count -lt 2) {
-            Write-Verbose -Message "Volume Status is encrypted, but BitLocker only has one key protector (TPM)"
-            Write-Verbose -Message "Adding a RecoveryPasswordProtector"
-            manage-bde -on $OSDrive -UsedSpaceOnly -rp
+        # Account for an uncrypted drive 
+        if ($bdeProtect.VolumeStatus -eq "FullyDecrypted" -or $bdeProtect.KeyProtector.Count -lt 1) {
+            
+            # Enable Bitlocker using TPM
+            Write-Verbose -Message "Enabling BitLocker due to FullyDecrypted status or KeyProtector count less than 1"
+            Enable-BitLocker -MountPoint $OSDrive -TpmProtector -SkipHardwareTest -UsedSpaceOnly -ErrorAction "Continue"
+            Enable-BitLocker -MountPoint $OSDrive -RecoveryPasswordProtector -SkipHardwareTest
+        }  
+        elseif ($bdeProtect.VolumeStatus -eq "FullyEncrypted" -or $bdeProtect.VolumeStatus -eq "UsedSpaceOnly") {
+            
+            # $bdeProtect.ProtectionStatus -eq "Off" - This catches the Wait State
+            if ($bdeProtect.KeyProtector.Count -lt 2) {
+                Write-Verbose -Message "Volume Status is encrypted, but BitLocker only has one key protector (TPM)"
+                Write-Verbose -Message "Adding a RecoveryPasswordProtector"
+                manage-bde -on $OSDrive -UsedSpaceOnly -rp
+            }
+            else {
+                Write-Verbose -Message "BitLocker is in Wait state - running manage-bde -on -UsedSpaceOnly"
+                manage-bde -on $OSDrive -UsedSpaceOnly
+            }
         }
-        else {
-            Write-Verbose -Message "BitLocker is in Wait state - running manage-bde -on -UsedSpaceOnly"
-            manage-bde -on $OSDrive -UsedSpaceOnly
+                    
+        # Check if we can use BackupToAAD-BitLockerKeyProtector commandlet
+        if (Get-Command -Name "BackupToAAD-BitLockerKeyProtector" -ErrorAction "SilentlyContinue") {
+            
+            # BackupToAAD-BitLockerKeyProtector commandlet exists
+            Write-Verbose -Message "Saving Key to AAD using BackupToAAD-BitLockerKeyProtector"
+            $BLV = Get-BitLockerVolume -MountPoint $OSDrive | Select-Object *
+            If ($Null -ne $BLV.KeyProtector) {
+                BackupToAAD-BitLockerKeyProtector -MountPoint $OSDrive -KeyProtectorId $BLV.KeyProtector[1].KeyProtectorId
+            }
+            Else {
+                Write-Error "'Get-BitLockerVolume' failed to retrieve drive encryption details for $OSDrive"
+            }
         }
+        else { 
+            # BackupToAAD-BitLockerKeyProtector commandlet not available, using other mechanism
+            Write-Verbose -Message "BackupToAAD-BitLockerKeyProtector not available"
+            Write-Verbose -Message "Saving Key to AAD using Enterprise Registration API"
+            
+            # Get the AAD Machine Certificate
+            $cert = Get-ChildItem -Path "Cert:\LocalMachine\My\" | Where-Object { $_.Issuer -match "CN=MS-Organization-Access" }
+
+            # Obtain the AAD Device ID from the certificate
+            $id = $cert.Subject.Replace("CN=", "")
+
+            # Obtain the Tenant ID from the certificate thumbprint
+            $tenantid = ($cert.Thumbprint).Replace("-","")
+
+            # Get the tenant name from the registry
+            $tenant = (Get-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\CloudDomainJoin\JoinInfo\$($tenantid)").UserEmail.Split('@')[1]
+
+            # Create the URL to post the data to based on the tenant and device information
+            $url = "https://enterpriseregistration.windows.net/manage/$tenant/device/$($id)?api-version=1.0"
+
+            # Generate the body to send to AAD containing the recovery information
+            Write-Verbose -Message "Saving key protector to AAD for self-service recovery by manually posting it to:"
+            Write-Verbose -Message "`t$url"
+            
+            # Get the BitLocker key information from WMI
+            (Get-BitLockerVolume -MountPoint $OSDrive).KeyProtector | Where-Object { $_.KeyProtectorType -eq 'RecoveryPassword' } | ForEach-Object {
+                $key = $_
+                $body = "{""key"":""$($key.RecoveryPassword)"",""kid"":""$($key.KeyProtectorId.replace('{','').Replace('}',''))"",""vol"":""OSV""}"
+                Write-Verbose -Message "KeyProtectorId : $($key.KeyProtectorId) key: $($key.RecoveryPassword)"
+                    
+                # Post the data to the URL and sign it with the AAD Machine Certificate
+                $req = Invoke-WebRequest -Uri $url -Body $body -UseBasicParsing -Method "Post" -UseDefaultCredentials -Certificate $cert
+                $req.RawContent
+                Write-Verbose -Message " -- Key save web request sent to AAD - Self-Service Recovery should work"
+            }
+        }
+
+        # In case we had to encrypt, turn it on for any enabled volume
+        Get-BitLockerVolume | Resume-BitLocker
+
+        # --------------------------------------------------------------------------
+        #  Finish - Let's dump the ending point
+        # --------------------------------------------------------------------------
+        Write-Verbose -Message "ENDING POINT:  Get-BitLockerVolume $OSDrive"
+        $bdeProtect = Get-BitLockerVolume $OSDrive 
+    } 
+    Else{
+        Write-Verbose -Message "Error enabling WinRE, further investigation required"
     }
-				
-    # Check if we can use BackupToAAD-BitLockerKeyProtector commandlet
-    if (Get-Command -Name "BackupToAAD-BitLockerKeyProtector" -ErrorAction "SilentlyContinue") {
-        
-        # BackupToAAD-BitLockerKeyProtector commandlet exists
-        Write-Verbose -Message "Saving Key to AAD using BackupToAAD-BitLockerKeyProtector"
-        $BLV = Get-BitLockerVolume -MountPoint $OSDrive | Select-Object *
-        If ($Null -ne $BLV.KeyProtector) {
-            BackupToAAD-BitLockerKeyProtector -MountPoint $OSDrive -KeyProtectorId $BLV.KeyProtector[1].KeyProtectorId
-        }
-        Else {
-            Write-Error "'Get-BitLockerVolume' failed to retrieve drive encryption details for $OSDrive"
-        }
-    }
-    else { 
-        # BackupToAAD-BitLockerKeyProtector commandlet not available, using other mechanism
-        Write-Verbose -Message "BackupToAAD-BitLockerKeyProtector not available"
-        Write-Verbose -Message "Saving Key to AAD using Enterprise Registration API"
-        
-        # Get the AAD Machine Certificate
-        $cert = Get-ChildItem -Path "Cert:\LocalMachine\My\" | Where-Object { $_.Issuer -match "CN=MS-Organization-Access" }
-
-        # Obtain the AAD Device ID from the certificate
-        $id = $cert.Subject.Replace("CN=", "")
-
-        # Obtain the Tenant ID from the certificate thumbprint
-        $tenantid = ($cert.Thumbprint).Replace("-","")
-
-        # Get the tenant name from the registry
-        $tenant = (Get-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\CloudDomainJoin\JoinInfo\$($tenantid)").UserEmail.Split('@')[1]
-
-        # Create the URL to post the data to based on the tenant and device information
-        $url = "https://enterpriseregistration.windows.net/manage/$tenant/device/$($id)?api-version=1.0"
-
-        # Generate the body to send to AAD containing the recovery information
-        Write-Verbose -Message "Saving key protector to AAD for self-service recovery by manually posting it to:"
-        Write-Verbose -Message "`t$url"
-        
-        # Get the BitLocker key information from WMI
-        (Get-BitLockerVolume -MountPoint $OSDrive).KeyProtector | Where-Object { $_.KeyProtectorType -eq 'RecoveryPassword' } | ForEach-Object {
-            $key = $_
-            $body = "{""key"":""$($key.RecoveryPassword)"",""kid"":""$($key.KeyProtectorId.replace('{','').Replace('}',''))"",""vol"":""OSV""}"
-            Write-Verbose -Message "KeyProtectorId : $($key.KeyProtectorId) key: $($key.RecoveryPassword)"
-				
-            # Post the data to the URL and sign it with the AAD Machine Certificate
-            $req = Invoke-WebRequest -Uri $url -Body $body -UseBasicParsing -Method "Post" -UseDefaultCredentials -Certificate $cert
-            $req.RawContent
-            Write-Verbose -Message " -- Key save web request sent to AAD - Self-Service Recovery should work"
-        }
-    }
-
-    # In case we had to encrypt, turn it on for any enabled volume
-    Get-BitLockerVolume | Resume-BitLocker
-
-    # --------------------------------------------------------------------------
-    #  Finish - Let's dump the ending point
-    # --------------------------------------------------------------------------
-    Write-Verbose -Message "ENDING POINT:  Get-BitLockerVolume $OSDrive"
-    $bdeProtect = Get-BitLockerVolume $OSDrive 
-} 
+}
 catch { 
     Write-Error "Error while setting up AAD Bitlocker, make sure that you are AAD joined and are running the cmdlet as an admin: $_" 
 }
