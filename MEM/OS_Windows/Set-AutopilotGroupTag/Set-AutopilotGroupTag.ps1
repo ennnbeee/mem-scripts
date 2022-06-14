98cd61e74a10562c491e8f4633a01f023919d861
ennnbeee <nickjbenton@gmail.com>
2022-06-14 17:17:01 +0100

    Updated Autopilot Group Tag script

--- MEM/OS_Windows/Set-AutopilotGroupTag/Set-AutopilotGroupTag.ps1
+++ MEM/OS_Windows/Set-AutopilotGroupTag/Set-AutopilotGroupTag.ps1
@@ -1,74 +1,354 @@
-# Set the Group Tag
-$GroupTag = "AAD-ASSIGNED-STAFF"
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory = $true)]
+    [ValidateSet('CSV', 'Online')]
+    [string]$Method,
+    [Parameter(Mandatory = $true)]
+    [string]$DefaultGroupTag
+)
 
-Connect-MSGraph
-Update-MSGraphEnvironment -SchemaVersion "Beta" -Quiet
-Connect-MSGraph -Quiet
-Import-Module azuread
-Connect-AzureAd
-Import-Module -Name WindowsAutoPilotIntune
+## Functions
+function Get-AuthToken {
 
-# Get all autopilot devices (even if more than 1000)
-$autopilotDevices = Invoke-MSGraphRequest -HttpMethod GET -Url "deviceManagement/windowsAutopilotDeviceIdentities" | Get-MSGraphAllPages
-
-# Display gridview to show devices
-#$selectedAutopilotDevices =  $autopilotDevices | Out-GridView -OutputMode Multiple -Title "Select Windows Autopilot entities to update"
-
-# Arrays to play with
-$aaddeviceids = New-Object -TypeName System.Collections.ArrayList
-$apdeviceids = New-Object -TypeName System.Collections.ArrayList
-$apdeviceserials = New-Object -TypeName System.Collections.ArrayList
-# Import CSV of devices to update
-
-$apDevicesCSV = Import-Csv -Path "C:\Users\Nick-Benton\Downloads\wapdevices.csv"
-foreach($apDeviceCSV in $apDevicesCSV){
-    #$aaddevice = Get-AzureADDevice -Filter "DisplayName eq '$($apDeviceCSV.displayName)'"
-    #$aaddeviceids.AddRange(@(
-    #    $aaddevice.DeviceID
-    #))
-    if($apDeviceCSV.'Group tag' -eq ""){
-        
-        $apdeviceserials.AddRange(@(
-        $apDeviceCSV.'Serial number'
-        ))
+    <#
+    .SYNOPSIS
+    This function is used to authenticate with the Graph API REST interface
+    .DESCRIPTION
+    The function authenticate with the Graph API Interface with the tenant name
+    .EXAMPLE
+    Get-AuthToken
+    Authenticates you with the Graph API interface
+    .NOTES
+    NAME: Get-AuthToken
+    #>
+    
+    [cmdletbinding()]
+    
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        $User
+    )
+    
+    $userUpn = New-Object "System.Net.Mail.MailAddress" -ArgumentList $User
+    
+    $tenant = $userUpn.Host
+    
+    Write-Host "Checking for AzureAD module..."
+    
+    $AadModule = Get-Module -Name "AzureAD" -ListAvailable
+    
+    if ($null -eq $AadModule) {
+    
+        Write-Host "AzureAD PowerShell module not found, looking for AzureADPreview"
+        $AadModule = Get-Module -Name "AzureADPreview" -ListAvailable
+    
     }
-
+    
+    if ($null -eq $AadModule) {
+        write-host
+        write-host "AzureAD Powershell module not installed..." -f Red
+        write-host "Install by running 'Install-Module AzureAD' or 'Install-Module AzureADPreview' from an elevated PowerShell prompt" -f Yellow
+        write-host "Script can't continue..." -f Red
+        write-host
+        exit
+    }
+    
+    # Getting path to ActiveDirectory Assemblies
+    # If the module count is greater than 1 find the latest version
+    
+    if ($AadModule.count -gt 1) {
+    
+        $Latest_Version = ($AadModule | Select-Object version | Sort-Object)[-1]
+    
+        $aadModule = $AadModule | Where-Object { $_.version -eq $Latest_Version.version }
+    
+        # Checking if there are multiple versions of the same module found
+    
+        if ($AadModule.count -gt 1) {
+    
+            $aadModule = $AadModule | select -Unique
+    
+        }
+    
+        $adal = Join-Path $AadModule.ModuleBase "Microsoft.IdentityModel.Clients.ActiveDirectory.dll"
+        $adalforms = Join-Path $AadModule.ModuleBase "Microsoft.IdentityModel.Clients.ActiveDirectory.Platform.dll"
+    
+    }
+    
+    else {
+    
+        $adal = Join-Path $AadModule.ModuleBase "Microsoft.IdentityModel.Clients.ActiveDirectory.dll"
+        $adalforms = Join-Path $AadModule.ModuleBase "Microsoft.IdentityModel.Clients.ActiveDirectory.Platform.dll"
+    
+    }
+    
+    [System.Reflection.Assembly]::LoadFrom($adal) | Out-Null
+    
+    [System.Reflection.Assembly]::LoadFrom($adalforms) | Out-Null
+    
+    $clientId = "d1ddf0e4-d672-4dae-b554-9d5bdfd93547"
+    
+    $redirectUri = "urn:ietf:wg:oauth:2.0:oob"
+    
+    $resourceAppIdURI = "https://graph.microsoft.com"
+    
+    $authority = "https://login.microsoftonline.com/$Tenant"
+    
+    try {
+    
+        $authContext = New-Object "Microsoft.IdentityModel.Clients.ActiveDirectory.AuthenticationContext" -ArgumentList $authority
+    
+        # https://msdn.microsoft.com/en-us/library/azure/microsoft.identitymodel.clients.activedirectory.promptbehavior.aspx
+        # Change the prompt behaviour to force credentials each time: Auto, Always, Never, RefreshSession
+    
+        $platformParameters = New-Object "Microsoft.IdentityModel.Clients.ActiveDirectory.PlatformParameters" -ArgumentList "Auto"
+    
+        $userId = New-Object "Microsoft.IdentityModel.Clients.ActiveDirectory.UserIdentifier" -ArgumentList ($User, "OptionalDisplayableId")
+    
+        $authResult = $authContext.AcquireTokenAsync($resourceAppIdURI, $clientId, $redirectUri, $platformParameters, $userId).Result
+    
+        # If the accesstoken is valid then create the authentication header
+    
+        if ($authResult.AccessToken) {
+    
+            # Creating header for Authorization token
+    
+            $authHeader = @{
+                'Content-Type'  = 'application/json'
+                'Authorization' = "Bearer " + $authResult.AccessToken
+                'ExpiresOn'     = $authResult.ExpiresOn
+            }
+    
+            return $authHeader
+    
+        }
+    
+        else {
+    
+            Write-Host
+            Write-Host "Authorization Access Token is null, please re-run authentication..." -ForegroundColor Red
+            Write-Host
+            break
+    
+        }
+    
+    }
+    
+    catch {
+    
+        write-host $_.Exception.Message -f Red
+        write-host $_.Exception.ItemName -f Red
+        write-host
+        break
+    
+    }
+    
 }
 
-foreach($autopilotDevice in $autopilotDevices){
-    if($autopilotDevice.groupTag -eq ""){
-        foreach($apdeviceserial in $apdeviceserials){
-            if($apdeviceserial -eq $autopilotDevice.serialNumber){
-                Set-AutopilotDevice -id $autopilotDevice.id -groupTag $groupTag
-                write-host -ForegroundColor Green "Group tag $groupTag assigned to $apdeviceserial"
-            }
+Function Get-AutopilotDevices() {
+
+    <#
+    .SYNOPSIS
+    This function is used to get autopilot devices via the Graph API REST interface
+    .DESCRIPTION
+    The function connects to the Graph API Interface and gets any autopilot devices
+    .EXAMPLE
+    Get-AutopilotDevices
+    Returns any autopilot devices
+    .NOTES
+    NAME: Get-AutopilotDevices
+    #>
+    
+    $graphApiVersion = "Beta"
+    $Resource = "deviceManagement/windowsAutopilotDeviceIdentities"
+    
+    try {
+    
+        $uri = "https://graph.microsoft.com/$graphApiVersion/$($Resource)"
+            (Invoke-RestMethod -Uri $uri -Headers $authToken -Method Get).Value
+        
+    }
+    
+    catch {
+    
+        $ex = $_.Exception
+        $errorResponse = $ex.Response.GetResponseStream()
+        $reader = New-Object System.IO.StreamReader($errorResponse)
+        $reader.BaseStream.Position = 0
+        $reader.DiscardBufferedData()
+        $responseBody = $reader.ReadToEnd();
+        Write-Host "Response content:`n$responseBody" -f Red
+        Write-Error "Request to $Uri failed with HTTP Status $($ex.Response.StatusCode) $($ex.Response.StatusDescription)"
+        write-host
+        break
+    
+    }
+    
+}
+
+Function Set-AutopilotDevice() {
+
+    <#
+    .SYNOPSIS
+    This function is used to set autopilot devices properties via the Graph API REST interface
+    .DESCRIPTION
+    The function connects to the Graph API Interface and sets autopilot device properties
+    .EXAMPLE
+    Set-AutopilotDevice
+    Returns any autopilot devices
+    .NOTES
+    NAME: Set-AutopilotDevice
+    #>
+
+    [CmdletBinding()]
+    param(
+        $Id,
+        $GroupTag
+    )
+    
+    $graphApiVersion = "Beta"
+    $Resource = "deviceManagement/windowsAutopilotDeviceIdentities/$Id/updateDeviceProperties"
+
+    try {
+
+        if (!$id) {
+            write-host "No Autopilot device Id specified, specify a valid Autopilot device Id" -f Red
+            break
+        }
+
+        if (!$GroupTag) {
+            $GroupTag = Read-host "No Group Tag specified, specify a Group Tag"
+        }
+
+        $Autopilot = New-Object -TypeName psobject
+        $Autopilot | Add-Member -MemberType NoteProperty -Name 'groupTag' -Value $GroupTag
+
+        $JSON = $Autopilot | ConvertTo-Json -Depth 3
+        # POST to Graph Service
+        $uri = "https://graph.microsoft.com/$graphApiVersion/$($Resource)"
+        Invoke-RestMethod -Uri $uri -Headers $authToken -Method Post -Body $JSON -ContentType "application/json"
+        write-host "Successfully added '$GroupTag' to device" -ForegroundColor Green
+        
+    }
+    
+    catch {
+    
+        $ex = $_.Exception
+        $errorResponse = $ex.Response.GetResponseStream()
+        $reader = New-Object System.IO.StreamReader($errorResponse)
+        $reader.BaseStream.Position = 0
+        $reader.DiscardBufferedData()
+        $responseBody = $reader.ReadToEnd();
+        Write-Host "Response content:`n$responseBody" -f Red
+        Write-Error "Request to $Uri failed with HTTP Status $($ex.Response.StatusCode) $($ex.Response.StatusDescription)"
+        write-host
+        break
+    
+    }
+    
+}
+
+#region Authentication
+
+write-host
+
+# Checking if authToken exists before running authentication
+if ($global:authToken) {
+
+    # Setting DateTime to Universal time to work in all timezones
+    $DateTime = (Get-Date).ToUniversalTime()
+
+    # If the authToken exists checking when it expires
+    $TokenExpires = ($authToken.ExpiresOn.datetime - $DateTime).Minutes
+
+    if ($TokenExpires -le 0) {
+
+        write-host "Authentication Token expired" $TokenExpires "minutes ago" -ForegroundColor Yellow
+        write-host
+
+        # Defining User Principal Name if not present
+
+        if ($null -eq $User -or $User -eq "") {
+
+            $User = Read-Host -Prompt "Please specify your user principal name for Azure Authentication"
+            Write-Host
 
         }
+
+        $global:authToken = Get-AuthToken -User $User
+
     }
-    else{
-        write-host -ForegroundColor Cyan "Group tag $($autopilotDevice.groupTag) already assigned to $($autopilotDevice.serialNumber)"
-    }
-
-}    
-
-
-$selectedAutopilotDevices | ForEach-Object {
-
-    $autopilotDevice = $PSItem
-
-    # Change names according to your environment
-    $autopilotDevice.groupTag = $GroupTag
-    #$autopilotDevice.orderIdentifier = "ORDER1234" | updating orderidentifier is currently not supported
-
-    $requestBody=
-@"
-    {
-        groupTag: `"$($autopilotDevice.groupTag)`",
-    }
-"@
-    Write-Output "Updating entity: $($autopilotDevice.id) | groupTag: $($autopilotDevice.groupTag) | orderIdentifier: $($autopilotDevice.orderIdentifier)"
-    Invoke-MSGraphRequest -HttpMethod POST -Content $requestBody -Url "deviceManagement/windowsAutopilotDeviceIdentities/$($autopilotDevice.id)/UpdateDeviceProperties" 
 }
 
-# Invoke an autopilot service sync
-Invoke-MSGraphRequest -HttpMethod POST -Url "deviceManagement/windowsAutopilotSettings/sync"
+# Authentication doesn't exist, calling Get-AuthToken function
+
+else {
+
+    if ($null -eq $User -or $User -eq "") {
+
+        $User = Read-Host -Prompt "Please specify your user principal name for Azure Authentication"
+        Write-Host
+
+    }
+
+    # Getting the authorization token
+    $global:authToken = Get-AuthToken -User $User
+
+}
+
+#endregion
+
+# Script Start
+# Get Devices
+if ($Method -eq 'CSV') {
+    $CSVPath = Read-host "Please provide the path to the CSV file containing a list of device serial numbers and new Group Tag  e.g. C:\temp\devices.csv"
+
+    if (!(Test-Path "$CSVPath")) {
+        Write-Host "Import Path for CSV file doesn't exist" -ForegroundColor Red
+        Write-Host "Script can't continue" -ForegroundColor Red
+        Write-Host
+        break
+        
+    }
+    else {
+        $AutopilotDevices = Import-Csv -Path $CSVPath
+    }
+}
+elseif ($Method -eq 'Online') {
+    Write-Host "Getting all Autopilot devices without a Group Tag" -ForegroundColor Cyan
+    $AutopilotDevices = Get-AutopilotDevices | Where-Object { ($null -eq $_.groupTag) -or ($_.groupTag) -eq ''  }
+}
+
+# Sets Group Tag
+foreach ($AutopilotDevice in $AutopilotDevices) {
+
+    $id = $AutopilotDevice.id
+    if (!$id) {
+        Write-host "No Autopilot Device Id, getting Id from Graph" -ForegroundColor Cyan
+        $id = (Get-AutopilotDevices | Where-Object { ($_.serialNumber -eq $AutopilotDevice.serialNumber) }).id
+        Write-Host "ID:'$Id' found for device with serial '$($AutopilotDevice.Serialnumber)'" -ForegroundColor Green
+    }
+
+    if ($Method -eq 'CSV') {
+        $GroupTag = $AutopilotDevice.groupTag
+        if (!$GroupTag) {
+            Write-host "No Autopilot Device Group Tag found in CSV" -ForegroundColor Cyan
+            $GroupTag = Read-Host 'Please enter the group tag for device with serial '$AutopilotDevice.serialNumber' now:'
+        }
+    }
+
+    elseif ($Method -eq 'Online') {
+        $GroupTag = $DefaultGroupTag
+    }
+
+    try {
+        Set-AutopilotDevice -Id $id -GroupTag $GroupTag
+        write-host "Group tag: '$GroupTag' set for device with serial '$($AutopilotDevice.Serialnumber)'" -ForegroundColor Green
+    }
+    catch {
+        write-host "Group tag: '$GroupTag' not set for device with serial '$($AutopilotDevice.Serialnumber)'" -ForegroundColor Red
+    }
+
+
+}
